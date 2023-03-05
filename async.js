1560c847881c70008e239d26d3eedf7e7813a276
evo264714 <mh264714@gmail.com>
2023-03-05 18:45:29 +0600

    second

--- async.js
+++ async.js
@@ -1,6 +1,6 @@
 console.log(1);
 console.log(2);
-setTimeout(() =>{
+const timeoutId = setTimeout(() =>{
     console.log('Lazy Logged');
 }, 4000)
 console.log(4);
@@ -10,3 +10,7 @@ console.log(6);
 function doSomething(){
     console.log(3);
 }
+let num = 0;
+const intervalId = setInterval(() =>{
+    console.log(num++);
+}, 1000)
